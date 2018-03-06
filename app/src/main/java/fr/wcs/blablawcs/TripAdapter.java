5ac1b06742a078cde9f0c7349c6a5d2181cc1ccf
Coralie Delplace <coralie.delplace@gmail.com>
2018-03-06 21:19:03 +0100

    rectification nom de variable et fichier xml

--- app/src/main/java/fr/wcs/blablawcs/TripAdapter.java
+++ app/src/main/java/fr/wcs/blablawcs/TripAdapter.java
@@ -7,10 +7,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -35,7 +33,7 @@ public class TripAdapter extends ArrayAdapter<TripModel> {
             TripModel trip = (TripModel) getItem(position);
             // Check if an existing view is being reused, otherwise inflate the view
             if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.idem_trip, parent, false);
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_trip, parent, false);
             }
             TextView textFirstname = (TextView)convertView.findViewById(R.id.textView_firstname);
             TextView textLastname = (TextView)convertView.findViewById(R.id.textView_lastname);
